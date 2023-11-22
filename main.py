0d7b4fa0164e604c42064f6550353fa616b11cda
Vlad <89295404+Virashu@users.noreply.github.com>
2023-11-22 18:53:15 +0300

    👺👺👺

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ class MyWidget(UI):
         self.update()
 
     def draw_circle(self, qp):
-        qp.setPen(QPen(QColor(255, 255, 0, 255), 8))
+        qp.setPen(
+            QPen(QColor(randint(0, 255), randint(0, 255), randint(0, 255), 255), 8)
+        )
         qp.setBrush(QColor(0, 0, 0, 0))
         x, y, d = randint(0, 300), randint(0, 300), randint(10, 30)
         qp.drawEllipse(x, y, d, d)
